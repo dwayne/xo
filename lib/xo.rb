3e5496157b39a4e25d32e7e26d0c8ff657c8d881
Dwayne R. Crooks <me@dwaynecrooks.com>
2014-05-05 04:09:11 -0400

    Remove unused code

--- lib/xo.rb
+++ lib/xo.rb
@@ -1,24 +1,3 @@
-module XO
-
-  X = :x
-  O = :o
-
-  def self.is_token?(val)
-    [X, O].include?(val)
-  end
-
-  def self.other_token(token)
-    token == X ? O : (token == O ? X : token)
-  end
-
-  class << self
-    alias_method :is_player?, :is_token?
-    alias_method :other_player, :other_token
-  end
-
-  class Position < Struct.new(:row, :column); end
-end
-
 require 'xo/grid'
 require 'xo/evaluator'
 require 'xo/engine'
